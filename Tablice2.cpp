c7e124f719d1c44d3ceba912142a3f3eec512d11
BoiSad <92589705+BoiSad@users.noreply.github.com>
2021-12-06 20:50:04 +0100

    Add files via upload

---
+++ Tablice2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	//zad 1
+	//char zdanie[] = { "lot" };// inicjalizacja łancuchów znaków
+
+	//char zdanie[] = { 'l', 'o' 't' }; //inicjalizacja zbiorcza
+
+	//zad 2
+
+	/*int liczby[100];
+	char znaki[100];
+	cin >> liczby;
+	cin >> znaki;*/
+
+	//zad 3
+
+	/* char tab[] = "wiosna";
+	cout << "liczba znakow: " << size(tab); */
+
+	//zad 4
+
+		/*string ciag("Karol ma psa");
+		string ciag_drugi(ciag);
+		cout << ciag << endl << ciag_drugi;
+
+		cin.get(); */
+
+	//zad 5
+	/*int ocena;
+	string nazwa_ulicy, numer_domu,staz;
+	cout << "podaj nazwe ulicy na ktorej mieszakasz: ";
+	getline(cin, nazwa_ulicy);
+	cout << "podaj nr domu: ";
+	cin >> numer_domu;
+	cout << "Na ile oceniasz swoje umiejetnosci programowania w skali [2 - 5]: ";
+	cin >> ocena;
+	ocena = ocena - 1;
+	cout << "twoj staz programistyczny: ";
+	cin >> staz;
+	cout << "Adres: "; cout << nazwa_ulicy; cout << numer_domu << endl;
+	cout << "Ocena: "; cout << ocena << endl;
+	cout << "Staz: "; cout << staz; cout << " dni"; */
+
+
+}
